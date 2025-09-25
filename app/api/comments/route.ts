79849f8af47753cd8e15fd363ed24553677a5cd9
Dickson Boateng <dicksonboateng67@gmail.com>
2025-09-25 02:31:00 +0000

    implement POST route to create comments

--- app/api/comments/route.ts
+++ app/api/comments/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { comments } from "@/app/lib/data";
 
+// GET all comments or filter by search query
 export async function GET(request: NextRequest) {
     const {searchParams} = new URL(request.url);
     const search = searchParams.get("search");
@@ -10,4 +11,13 @@ export async function GET(request: NextRequest) {
         return NextResponse.json(filteredComments);
     }
     return NextResponse.json(comments);
+}
+
+// POST a new comment
+export async function POST(request: NextRequest) {
+    const body = await request.json();
+    const newComment = {id: Date.now(), text: body.text};
+
+    comments.push(newComment);
+    return NextResponse.json(newComment, {status: 201});
 }
